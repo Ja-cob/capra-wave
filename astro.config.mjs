690b2f3bec0e6c78abe97ee96ee51a5eb25ecb50
Jacob Bass <37024840+Ja-cob@users.noreply.github.com>
2024-06-13 16:01:18 -0500

    updated icon list and config 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "",
+  site: "https://caprawave.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
